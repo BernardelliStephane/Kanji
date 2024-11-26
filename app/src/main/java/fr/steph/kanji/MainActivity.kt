6c35b93404c5b23dde6bca15ebf4d26d6e2a5d56
BernardelliStephane <steph.bernardelli@hotmail.fr>
2024-11-26 18:47:19 +0100

    First commit

---
+++ app/src/main/java/fr/steph/kanji/MainActivity.kt
@@ -0,0 +1,17 @@
+package fr.steph.kanji
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import fr.steph.kanji.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
