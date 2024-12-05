eedeaae1b0e7481ea52d850d2c2c63a9a15d7f62
BernardelliStephane <steph.bernardelli@hotmail.fr>
2024-12-05 22:54:55 +0100

    Navigation to DictionaryFragment

--- app/src/main/java/fr/steph/kanji/fragment/HomeFragment.kt
+++ app/src/main/java/fr/steph/kanji/fragment/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import fr.steph.kanji.R
 import fr.steph.kanji.databinding.FragmentHomeBinding
+import fr.steph.kanji.utils.extension.safeNavigate
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
@@ -15,6 +16,17 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentHomeBinding.bind(view)
+
+        initializeViews()
+    }
+
+    private fun initializeViews() {
+        binding.apply {
+            buttonDictionary.view.setOnClickListener {
+                val action = HomeFragmentDirections.actionHomeFragmentToDictionaryFragment()
+                safeNavigate(action)
+            }
+        }
     }
 
     override fun onDestroyView() {
