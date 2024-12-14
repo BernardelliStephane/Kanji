9a04e11bd7f45c689f30f7d7229e16888531eff7
BernardelliStephane <steph.bernardelli@hotmail.fr>
2024-12-14 20:01:06 +0100

    Hide expandedTitle when toolbar is collapsed

--- app/src/main/java/fr/steph/kanji/fragment/DictionaryFragment.kt
+++ app/src/main/java/fr/steph/kanji/fragment/DictionaryFragment.kt
@@ -15,6 +15,21 @@ class DictionaryFragment : Fragment(R.layout.fragment_dictionary) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentDictionaryBinding.bind(view)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.run {
+            appBar.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+                val range = appBarLayout.totalScrollRange
+                val displayRatio = (range + verticalOffset).toFloat() / range
+                expandedTitle.alpha =
+                    if(displayRatio < 0.5) 0F
+                    else 1 - ((1 - displayRatio) * 2)
+            }
+
+        }
     }
 
     override fun onDestroyView() {
