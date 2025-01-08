2f3114cde2c230bff32f8cabfde8818d4a8c8162
BernardelliStephane <steph.bernardelli@hotmail.fr>
2025-01-08 22:32:41 +0100

    Move Kanji logic to KanjiViewModel

--- app/src/main/java/fr/steph/kanji/ui/viewmodel/AddKanjiViewModel.kt
+++ app/src/main/java/fr/steph/kanji/ui/viewmodel/AddKanjiViewModel.kt
@@ -1,20 +1,9 @@
 package fr.steph.kanji.ui.viewmodel
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import fr.steph.kanji.R
 import fr.steph.kanji.data.model.Kanji
 import fr.steph.kanji.data.repository.KanjiRepository
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.launch
 
-const val FAILURE = -1L
-
-class AddKanjiViewModel(private val repo: KanjiRepository) : ViewModel() {
-
-    private val validationEventChannel = Channel<ValidationEvent>()
-    val validationEvents = validationEventChannel.receiveAsFlow()
+class AddKanjiViewModel(repo: KanjiRepository) : KanjiViewModel(repo) {
 
     var id = 0
     var character: String = ""
@@ -26,18 +15,5 @@ class AddKanjiViewModel(private val repo: KanjiRepository) : ViewModel() {
         // TODO Verify entries before upserting kanji
         val kanji = Kanji(id, character, kana, romaji, translation)
         upsertKanji(kanji)
-    }
-
-    private fun upsertKanji(kanji: Kanji) = viewModelScope.launch {
-        repo.upsertKanji(kanji).let {
-            if(it == FAILURE)
-                validationEventChannel.send(ValidationEvent.Failure(R.string.room_insertion_failure))
-            else validationEventChannel.send(ValidationEvent.Success)
-        }
-    }
-
-    sealed class ValidationEvent {
-        data class Failure (val failureMessage: Int): ValidationEvent()
-        data object Success: ValidationEvent()
     }
 }
--- app/src/main/java/fr/steph/kanji/ui/viewmodel/KanjiViewModel.kt
+++ app/src/main/java/fr/steph/kanji/ui/viewmodel/KanjiViewModel.kt
@@ -3,14 +3,22 @@ package fr.steph.kanji.ui.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import fr.steph.kanji.R
+import fr.steph.kanji.data.model.Kanji
 import fr.steph.kanji.data.repository.KanjiRepository
 import fr.steph.kanji.data.utils.enumeration.SortField
 import fr.steph.kanji.data.utils.enumeration.SortOrder
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+
+const val FAILURE = -1L
 
 @OptIn(ExperimentalCoroutinesApi::class)
 abstract class KanjiViewModel(private val repo: KanjiRepository): ViewModel() {
@@ -25,4 +33,20 @@ abstract class KanjiViewModel(private val repo: KanjiRepository): ViewModel() {
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
 
     val kanjis = _kanjis.asLiveData()
+
+    private val validationEventChannel = Channel<ValidationEvent>()
+    val validationEvents = validationEventChannel.receiveAsFlow()
+
+    fun upsertKanji(kanji: Kanji) = viewModelScope.launch {
+        repo.upsertKanji(kanji).let {
+            if(it == FAILURE)
+                validationEventChannel.send(ValidationEvent.Failure(R.string.room_insertion_failure))
+            else validationEventChannel.send(ValidationEvent.Success)
+        }
+    }
+
+    sealed class ValidationEvent {
+        data class Failure (val failureMessage: Int): ValidationEvent()
+        data object Success: ValidationEvent()
+    }
 }
