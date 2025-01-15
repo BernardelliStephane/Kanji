b4243b0579dcdf3c8f75352e2dad71a50481c3f2
BernardelliStephane <steph.bernardelli@hotmail.fr>
2025-01-15 12:20:35 +0100

    Update repository

--- app/src/main/java/fr/steph/kanji/data/dao/LexemeDao.kt
+++ app/src/main/java/fr/steph/kanji/data/dao/LexemeDao.kt
@@ -4,7 +4,6 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
-import fr.steph.kanji.data.model.Kanji
 import fr.steph.kanji.data.model.Lexeme
 import kotlinx.coroutines.flow.Flow
 
---
+++ app/src/main/java/fr/steph/kanji/data/repository/LexemeRepository.kt
@@ -0,0 +1,30 @@
+package fr.steph.kanji.data.repository
+
+import androidx.annotation.WorkerThread
+import fr.steph.kanji.data.dao.LexemeDao
+import fr.steph.kanji.data.model.Lexeme
+import fr.steph.kanji.data.utils.enumeration.SortOrder
+
+class LexemeRepository(private val lexemeDao: LexemeDao) {
+
+    suspend fun upsertLexeme(lexeme: Lexeme) =
+        lexemeDao.upsertLexeme(lexeme)
+
+    suspend fun deleteLexeme(lexeme: Lexeme) =
+        lexemeDao.deleteLexeme(lexeme)
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun lexemesOrderedById(sortOrder: SortOrder) =
+        lexemeDao.lexemesOrderedById(sortOrder.index)
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun lexemesOrderedByRomaji(sortOrder: SortOrder) =
+        lexemeDao.lexemesOrderedByRomaji(sortOrder.index)
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun lexemesOrderedByTranslation(sortOrder: SortOrder) =
+        lexemeDao.lexemesOrderedByMeaning(sortOrder.index)
+}
