70c63c532fabc144777f59c9b018ca2cecc2a50d
BernardelliStephane <steph.bernardelli@hotmail.fr>
2025-01-15 12:23:22 +0100

    Update database class

--- app/src/main/java/fr/steph/kanji/data/LexemeDatabase.kt
+++ app/src/main/java/fr/steph/kanji/data/LexemeDatabase.kt
@@ -5,26 +5,26 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import fr.steph.kanji.data.dao.KanjiDao
-import fr.steph.kanji.data.model.Kanji
+import fr.steph.kanji.data.dao.LexemeDao
+import fr.steph.kanji.data.model.Lexeme
 import fr.steph.kanji.data.utils.RoomTypeConverter
 
-@Database(entities = [Kanji::class], version = 1)
+@Database(entities = [Lexeme::class], version = 1)
 @TypeConverters(value = [RoomTypeConverter::class])
-abstract class KanjiDatabase: RoomDatabase() {
-    abstract fun dao(): KanjiDao
+abstract class LexemeDatabase: RoomDatabase() {
+    abstract fun dao(): LexemeDao
 
     companion object {
         @Volatile
-        private var INSTANCE: KanjiDatabase? = null
+        private var INSTANCE: LexemeDatabase? = null
 
-        fun getDatabase(context: Context): KanjiDatabase {
+        fun getDatabase(context: Context): LexemeDatabase {
             return INSTANCE ?: synchronized(this) {
 
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
-                    KanjiDatabase::class.java,
-                    "kanji_db"
+                    LexemeDatabase::class.java,
+                    "lexeme_db"
                 ).build()
 
                 INSTANCE = instance
