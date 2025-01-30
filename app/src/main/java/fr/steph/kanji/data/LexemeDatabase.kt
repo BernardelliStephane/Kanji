d710944a79ab97fcb8063ca3f805a5620e67813e
BernardelliStephane <steph.bernardelli@hotmail.fr>
2025-01-30 18:47:20 +0100

    Fix plurals resource

--- app/src/main/java/fr/steph/kanji/data/LexemeDatabase.kt
+++ app/src/main/java/fr/steph/kanji/data/LexemeDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import fr.steph.kanji.data.dao.LexemeDao
 import fr.steph.kanji.data.model.Lexeme
 
