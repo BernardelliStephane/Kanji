244691fa01b099955a5d0679e351a5071f73707c
BernardelliStephane <steph.bernardelli@hotmail.fr>
2025-02-13 21:22:52 +0100

    ViewModel now stores only selection size

--- app/src/main/java/fr/steph/kanji/ui/uistate/DictionaryFragmentState.kt
+++
@@ -1,6 +0,0 @@
-package fr.steph.kanji.ui.uistate
-
-data class DictionaryFragmentState(
-    var itemsSelected: Int = 0,
-    var isSelectionMode: Boolean = false,
-)
--- app/src/main/java/fr/steph/kanji/ui/viewmodel/DictionaryViewModel.kt
+++ app/src/main/java/fr/steph/kanji/ui/viewmodel/DictionaryViewModel.kt
@@ -1,27 +1,15 @@
 package fr.steph.kanji.ui.viewmodel
 
 import fr.steph.kanji.data.repository.LexemeRepository
-import fr.steph.kanji.ui.uistate.DictionaryFragmentState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class DictionaryViewModel(repo: LexemeRepository) : LexemeViewModel(repo) {
-
-    private val _uiState = MutableStateFlow(DictionaryFragmentState())
-    val uiState = _uiState.asStateFlow()
+    private val _selectionSize = MutableStateFlow(0)
+    val selectionSize = _selectionSize.asStateFlow()
 
     fun onSelectionChanged(selectionSize: Int) {
-        if(selectionSize == uiState.value.itemsSelected) return
-        _uiState.update { currentUiState ->
-            val selectionMode = uiState.value.isSelectionMode || selectionSize != 0
-            currentUiState.copy(itemsSelected = selectionSize, isSelectionMode = selectionMode)
-        }
-    }
-
-    fun onSelectionCleared() {
-        _uiState.update { currentUiState ->
-            currentUiState.copy(itemsSelected = 0, isSelectionMode = false)
-        }
+        _selectionSize.update { selectionSize }
     }
 }
