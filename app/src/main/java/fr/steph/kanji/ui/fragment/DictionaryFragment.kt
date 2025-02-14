fa5b88d39872767802f25d94d59f71761bf2e9af
BernardelliStephane <steph.bernardelli@hotmail.fr>
2025-02-14 19:26:55 +0100

    Swap CollapsingToolbar title for custom title view

--- app/src/main/java/fr/steph/kanji/ui/fragment/DictionaryFragment.kt
+++ app/src/main/java/fr/steph/kanji/ui/fragment/DictionaryFragment.kt
@@ -1,12 +1,11 @@
 package fr.steph.kanji.ui.fragment
 
-import android.content.res.ColorStateList
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.CheckBox
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -80,14 +79,7 @@ class DictionaryFragment : Fragment(R.layout.fragment_dictionary) {
                 val displayRatio = (range + verticalOffset).toFloat() / range
 
                 // Ranges from 0 to 255 as the displayRatio goes from 0 to 0.5 | 0 above 0.5
-                val opacity = if (displayRatio > 0.5) 0
-                else 255 - (((displayRatio * 255).toInt() * 2) - 255)
-
-                val color = Color.argb(opacity, 0, 0, 0)
-                val colorStateList = ColorStateList.valueOf(color)
-
-                collapsingToolbarLayout.setExpandedTitleTextColor(colorStateList)
-                collapsingToolbarLayout.setCollapsedTitleTextColor(color)
+                collapsedTitle.alpha = 1 - (displayRatio * 2)
 
                 // Ranges from 1 to 0 as the displayRatio goes from 1 to 0.5 | 0 below 0.5
                 expandedTitleLayout.alpha = 1 - ((1 - displayRatio) * 2)
@@ -147,10 +139,13 @@ class DictionaryFragment : Fragment(R.layout.fragment_dictionary) {
                         val title = if (!isSelectionActive) "Dictionary" else "$selectionSize selected"
 
                         expandedTitle.text = title
-                        collapsingToolbarLayout.title = title
+                        collapsedTitle.text = title
 
                         if (isDifferentMode) {
-                            dictionaryToolbar.navigationIcon?.alpha = if (isSelectionActive) 0 else 255
+                            dictionaryToolbar.navigationIcon =
+                                if (isSelectionActive) null
+                                else ResourcesCompat.getDrawable(resources, R.drawable.ic_back, null)
+
                             translationCount.isVisible = !isSelectionActive
                             selectAllLayout.isVisible = isSelectionActive
                             addLexeme.isVisible = !isSelectionActive
