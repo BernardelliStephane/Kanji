c5fb827e656730dcb13a7704f32b9f79df1a830a
BernardelliStephane <steph.bernardelli@hotmail.fr>
2025-02-17 18:22:53 +0100

    Map allSelected from selectionSize

--- app/src/main/java/fr/steph/kanji/ui/viewmodel/DictionaryViewModel.kt
+++ app/src/main/java/fr/steph/kanji/ui/viewmodel/DictionaryViewModel.kt
@@ -1,19 +1,23 @@
 package fr.steph.kanji.ui.viewmodel
 
+import androidx.lifecycle.viewModelScope
 import fr.steph.kanji.data.repository.LexemeRepository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 
 class DictionaryViewModel(repo: LexemeRepository) : LexemeViewModel(repo) {
     private val _selectionSize = MutableStateFlow(0)
     val selectionSize = _selectionSize.asStateFlow()
 
-    private val _allSelected = MutableStateFlow(false)
-    val allSelected = _allSelected.asStateFlow()
+    val allSelected = _selectionSize
+        .map { it == (lexemes.value?.size ?: 0) }
+        .stateIn(viewModelScope, SharingStarted.Lazily, false)
 
     fun onSelectionChanged(selectionSize: Int) {
         _selectionSize.update { selectionSize }
-        _allSelected.update { selectionSize == lexemes.value!!.size }
     }
 }
