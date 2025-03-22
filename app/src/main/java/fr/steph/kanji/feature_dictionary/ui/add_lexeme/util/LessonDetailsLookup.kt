977e18ed0e78c80af6a9eb92497e38fc071de2f6
BernardelliStephane <steph.bernardelli@hotmail.fr>
2025-03-22 22:19:09 +0100

    Add KDoc comments to ItemDetailsLookup implementations

--- app/src/main/java/fr/steph/kanji/feature_dictionary/ui/add_lexeme/util/LessonDetailsLookup.kt
+++ app/src/main/java/fr/steph/kanji/feature_dictionary/ui/add_lexeme/util/LessonDetailsLookup.kt
@@ -5,6 +5,15 @@ import androidx.recyclerview.selection.ItemDetailsLookup
 import androidx.recyclerview.widget.RecyclerView
 import fr.steph.kanji.feature_dictionary.ui.add_lexeme.adapter.LessonAdapter
 
+/**
+ * Custom [ItemDetailsLookup] implementation for retrieving details of a lesson item in a [RecyclerView].
+ *
+ * This class is used to provide item details for a specific lesson when interacting with the
+ * [RecyclerView] during touch events, such as when selecting or highlighting a lesson.
+ *
+ * It extends [ItemDetailsLookup] and overrides the [getItemDetails] method to retrieve the item
+ * details of the lesson under a given motion event (e.g., a touch event).
+ */
 class LessonDetailsLookup(private val recyclerView: RecyclerView) : ItemDetailsLookup<Long>() {
     override fun getItemDetails(event: MotionEvent): ItemDetails<Long>? {
         val view = recyclerView.findChildViewUnder(event.x, event.y)
--- app/src/main/java/fr/steph/kanji/feature_dictionary/ui/add_lexeme/util/LexemeDetailsLookup.kt
+++ app/src/main/java/fr/steph/kanji/feature_dictionary/ui/add_lexeme/util/LexemeDetailsLookup.kt
@@ -5,6 +5,15 @@ import androidx.recyclerview.selection.ItemDetailsLookup
 import androidx.recyclerview.widget.RecyclerView
 import fr.steph.kanji.feature_dictionary.ui.dictionary.adapter.LexemeAdapter
 
+/**
+ * Custom [ItemDetailsLookup] implementation for retrieving details of a lexeme item in a [RecyclerView].
+ *
+ * This class is used to provide item details for a specific lexeme when interacting with the
+ * [RecyclerView] during touch events, such as when selecting or highlighting a lexeme.
+ *
+ * It extends [ItemDetailsLookup] and overrides the [getItemDetails] method to retrieve the item
+ * details of the lexeme under a given motion event (e.g., a touch event).
+ */
 class LexemeDetailsLookup(private val recyclerView: RecyclerView) : ItemDetailsLookup<Long>() {
     override fun getItemDetails(event: MotionEvent): ItemDetails<Long>? {
         val view = recyclerView.findChildViewUnder(event.x, event.y)
