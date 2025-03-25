3d275bc5dec8541b6864ea97b50bfeeea7073fa9
BernardelliStephane <steph.bernardelli@hotmail.fr>
2025-03-25 18:01:09 +0100

    Add KDoc comment to setupDialogWindow method

--- app/src/main/java/fr/steph/kanji/core/util/extension/DialogFragment.kt
+++ app/src/main/java/fr/steph/kanji/core/util/extension/DialogFragment.kt
@@ -5,6 +5,12 @@ import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import androidx.fragment.app.DialogFragment
 
+/**
+ * Configures the dialog window appearance by allowing custom background usage and setting the
+ * dialog's width to MATCH_PARENT.
+ *
+ * @param alignBottom If `true`, the dialog will be aligned to the bottom of the screen. Default is `true`.
+ */
 fun DialogFragment.setupDialogWindow(alignBottom: Boolean = true) {
     dialog?.window?.apply {
         setLayout(MATCH_PARENT, WRAP_CONTENT)
